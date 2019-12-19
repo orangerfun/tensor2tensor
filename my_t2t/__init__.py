734033716abd7e5678346e9e728a6a3710aae16a
fangcheng <orangerfun@gmail.com>
2019-12-19 23:17:47 +0800

    add init file

---
+++ my_t2t/__init__.py
@@ -0,0 +1,6 @@
+from my_t2t import *
+from my_t2t.translate_zhen_general import *
+from my_t2t.translate_enzh_general import *
+from my_t2t.transformer import *
+from my_t2t.translate_enzh_fc import *
+from my_t2t.translate_enzh_bpe import *
